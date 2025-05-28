d89f07f58898809d6750c7eef3f4ee4801cef2ab
Gillian Barreto Arias <gillianbarretoarias@MacBook-Pro-de-Gillian.local>
2025-05-28 16:13:05 -0400

    refactor: change guards as a function

--- src/app/core/guards/auth-guard/auth-guard.ts
+++ src/app/core/guards/auth-guard/auth-guard.ts
@@ -1,23 +1,17 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 import { SessionService } from '@services';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private sessionService: SessionService,
-  ) {}
+export const AuthGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const sessionService = inject(SessionService);
 
-  public canActivate(): boolean {
-    if (!this.sessionService.isLogged()) {
-      this.sessionService.initStorage();
-      this.router.navigate(['login']);
-      return false;
-    }
-    return true;
+  if (!sessionService.isLogged()) {
+    sessionService.initStorage();
+    router.navigate(['login']);
+    return false;
   }
-}
+
+  return true;
+};
